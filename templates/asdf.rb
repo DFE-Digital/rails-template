d6cbde89758a3ab1eb2e0170cf246263261f3212
Misha Gorodnitzky <misaka@pobox.com>
2022-12-07 18:25:22 +0000

    Move adr and update asdf sections in README

--- templates/asdf.rb
+++ templates/asdf.rb
@@ -7,23 +7,37 @@ run "asdf plugin add yarn || true"
 run "asdf plugin add postgres || true"
 run "asdf install"
 
+postgres_version = get_tools_version_of("postgres")
+
 append_to_file(
   'README.md',
   <<~MD
     ## Setup
 
-    Install dependencies using your preferred method, using `asdf` or `rbenv` or
-    `nvm`. Example with `asdf`:
+    This project uses `asdf`. Use the following to install the required tools:
 
-    ```bash
+    ```sh
     # The first time
     brew install asdf # Mac-specific
     asdf plugin add ruby
     asdf plugin add nodejs
     asdf plugin add yarn
+    asdf plugin add postgres
 
     # To install (or update, following a change to .tool-versions)
     asdf install
     ```
+
+    When installing the `pg` gem, bundle changes directory outside of this
+    project directory, causing it lose track of which version of postgres has
+    been selected in the project's `.tool-versions` file. To ensure the `pg` gem
+    installs correctly, you'll want to set the version of postgres that `asdf`
+    will use:
+
+    ```sh
+    # Temporarily set the version of postgres to use to build the pg gem
+    ASDF_POSTGRES_VERSION=#{postgres_version} bundle install
+    ```
+
   MD
 )
