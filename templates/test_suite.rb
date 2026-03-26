926eeabeb54310222d54af8aa0cd62363fd7a6ad
David Young <david@dcyoung.dev>
2026-03-26 13:46:24 +0000

    Improved test suite setup
    Adds and configures RSpec, Capybara, Factory Bot and Shoulda Matchers.
    Creates binstub for RSpec
    Adds tests to CI setup

--- templates/test_suite.rb
+++ templates/test_suite.rb
@@ -1,8 +1,45 @@
-gem_group :test, :development do
+gem_group :development, :test do
   gem "rspec"
   gem "rspec-rails"
+  gem "capybara"
+  gem "factory_bot_rails"
+  gem "shoulda-matchers"
 end unless file_contains?("Gemfile", 'rspec-rails')
 
 run "bundle --quiet"
 
 generate("rspec:install") unless file_exists?(".rspec")
+
+run "bundle binstubs rspec-core"
+
+template('spec/support/capybara.rb', 'spec/support/capybara.rb')
+template('spec/support/factory_bot.rb', 'spec/support/factory_bot.rb')
+template('spec/support/shoulda_matchers.rb', 'spec/support/shoulda_matchers.rb')
+template('spec/support/time_helper.rb', 'spec/support/time_helper.rb')
+template('spec/requests/smoke_test_spec.rb', 'spec/requests/smoke_test_spec.rb')
+
+uncomment_lines 'spec/rails_helper.rb', /Rails.root.glob/
+uncomment_lines 'spec/rails_helper.rb', /config.infer_spec_type_from_file_location!/
+
+default_fixtures_config = <<-RUBY
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_paths = [
+    Rails.root.join('spec/fixtures')
+  ]
+RUBY
+
+removed_fixtures_config = <<-RUBY
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # config.fixture_paths = [
+  #   Rails.root.join('spec/fixtures')
+  # ]
+RUBY
+
+gsub_file("spec/rails_helper.rb", default_fixtures_config, removed_fixtures_config)
+
+ci_steps = <<-RUBY
+step "Tests: Rails", "bin/rspec --format documentation"
+
+RUBY
+
+insert_into_file "config/ci.rb", ci_steps, before: "# Optional:"
