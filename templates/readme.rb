48a49f4de425ef44e324fab90f0b4cf0477f2177
David Young <david@dcyoung.dev>
2026-03-27 11:15:56 +0000

    Improved readme for new projects

--- templates/readme.rb
+++ templates/readme.rb
@@ -1,22 +1,6 @@
 #!/usr/bin/env ruby
 
-say "  - Updating README"
+say "  - Setting up README"
 
-append_to_file(
-  'README.md',
-  <<~MD
-
-    ## Setup
-
-    ### Prerequisites
-
-    This project depends on:
-
-      - [Ruby](https://www.ruby-lang.org/)
-      - [Ruby on Rails](https://rubyonrails.org/)
-      - [NodeJS](https://nodejs.org/)
-      - [Yarn](https://yarnpkg.com/)
-      - [Postgres](https://www.postgresql.org/)
-
-  MD
-)
+remove_file('README.md', verbose: false)
+template('template_files/README.tt', 'README.md')
