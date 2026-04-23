abe3ad77a8b1e1edcc844d87c14638f87e5525e7
David Young <david@dcyoung.dev>
2026-04-23 13:36:39 +0100

    Adds DFE analytics setup (#42)
    * Added DFE Analytics gem and setup
    
    * Update templates/dfe_analytics.rb
    
    Co-authored-by: Robert Rees <rrees@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Robert Rees <rrees@users.noreply.github.com>

---
+++ templates/dfe_analytics.rb
@@ -0,0 +1,45 @@
+gem 'dfe-analytics', github: 'DFE-Digital/dfe-analytics', tag: 'v1.15.8'
+
+bundle_command("install --quiet")
+
+run "rails generate dfe:analytics:install"
+
+airbyte_config = <<-RUBY
+  # Set a config file for Airbyte
+  config.airbyte_stream_config_path = 'config/analytics_airbyte_stream_config.json'
+  
+RUBY
+# FIXME: Uncomment this when the version is updated
+# insert_into_file("config/initializers/dfe_analytics.rb", airbyte_config, after: "DfE::Analytics.configure do |config|\n" )
+
+azure_federated_auth_config = <<-RUBY
+  # FIXME: Review this setting to ensure it's appropriate for your application
+  # Use Azure Federated Auth [see docs](https://github.com/DFE-Digital/dfe-analytics?tab=readme-ov-file#31-workload-identity-federation) 
+  config.azure_federated_auth = true
+  
+RUBY
+gsub_file("config/initializers/dfe_analytics.rb", " # config.azure_federated_auth = false", azure_federated_auth_config)
+
+log_only_config = <<-RUBY
+  # FIXME: Review this setting to ensure it's appropriate for your application
+  config.log_only = Rails.env.local? 
+
+RUBY
+gsub_file("config/initializers/dfe_analytics.rb", " # config.log_only = true", log_only_config)
+
+async_config = <<-RUBY
+  # FIXME: Review this setting to ensure it's appropriate for your application
+  config.async = !Rails.env.local? 
+RUBY
+gsub_file("config/initializers/dfe_analytics.rb", " # config.async = true", async_config)
+
+queue_config = <<-RUBY
+  config.queue = :dfe_analytics
+RUBY
+gsub_file("config/initializers/dfe_analytics.rb", " # config.queue = :default", queue_config)
+
+inject_into_file(
+  "app/controllers/application_controller.rb",
+  "include DfE::Analytics::Requests\n".indent(2),
+  after: "class ApplicationController < ActionController::Base\n"
+)
